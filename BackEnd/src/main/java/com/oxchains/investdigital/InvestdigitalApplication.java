af58b515955e590cd383e1d45f44b7971554995c
cherrishccl <528050239@qq.com>
2017-12-12 17:56:01 +0800

    first commit

---
+++ BackEnd/src/main/java/com/oxchains/investdigital/InvestdigitalApplication.java
@@ -0,0 +1,12 @@
+package com.oxchains.investdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestdigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestdigitalApplication.class, args);
+	}
+}
---
+++ BackEnd/src/test/java/com/oxchains/investdigital/InvestdigitalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oxchains.investdigital;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvestdigitalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
