05121e59e040c45a0db829d7d7bce8a0f6d73694
vueuserfxl <fengxiaoli_007@126.com>
2017-12-12 18:08:39 +0800

    first commit

---
+++ FrontEnd/src/components/home.js
@@ -0,0 +1,24 @@
+/**
+ * Created by fengxiaoli on 2017/12/12.
+ */
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="clear">
+                <div className="headermain">
+                    <div className="bannertitle">
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ FrontEnd/src/index.js
@@ -0,0 +1,56 @@
+/**
+ * Created by fengxiaoli on 2017/12/12.
+ */
+import React from 'react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import Header from './components/common/header';
+import Footer from './components/common/footer';
+import Singin from './components/auth/signin';
+import Signinemail from './components/auth/signin_email';
+import Singup from './components/auth/signup';
+import Signupemail from './components/auth/signup_email';
+
+import Signout from './components/auth/signout';
+import Home from './components/home';
+
+const createStoreWithMiddleware = compose(
+    applyMiddleware(reduxThunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// If token exist, singin automatic
+if (token) {
+    store.dispatch({ type: AUTH_USER });
+}
+
+ReactDOM.render(
+    <Provider store={store} >
+        <BrowserRouter>
+            <div>
+                <main>
+                    <Header />
+                    <Switch>
+                        <Route path="/signin" component={Singin} />
+                        <Route path="/signinemail" component={Signinemail} />
+                        <Route path="/signup" component={Singup} />
+                        <Route path="/signupemail" component={Signupemail} />
+                        <Route path="/signout" component={Signout} />
+                        <Route path="/" component={Home} />
+                    </Switch>
+                    <Footer />
+                </main>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.wrapper')
+);
+
